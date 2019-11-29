dc1a469b190f405c61caa8136d7d54b0bc456bfa
unknown <evilp@DESKTOP-TS9S051.chinawyny.com>
2019-11-29 14:39:17 +0800

    delete setup files

--- build/lib/dtsp/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-"""
-@author : zhirui zhou
-@contact: evilpsycho42@gmail.com
-@time   : 2019/11/19 14:12
-"""
-from .simple_seq2seq import SimpleSeq2Seq
-from .simple_wavenet import SimpleWaveNet
-from .seq2seq import Seq2Seq
